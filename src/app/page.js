ba8529b06ac7ef3eb97902a28916effe35cef697
Eric <diego123@Eric.local>
2023-09-06 14:07:53 -0600

    create grid

--- src/app/page.js
+++ src/app/page.js
@@ -1,95 +1,42 @@
-import Image from 'next/image'
+
 import styles from './page.module.css'
 
+
 export default function Home() {
-  return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.js</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+  const images = [
+    "https://picsum.photos/200/300?image=1050",
+    //...
+    "https://picsum.photos/300/300?image=206",
+]
+  return(
+    <div className={styles.gallery}>
+        <div className={styles.galleryItem}>
+            <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTTS21m64ueFfh9cNHn-pKBcAVtJHzwjdUhzNhwxjk&s" alt="Image 2"/>
         </div>
-      </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore the Next.js 13 playground.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+        <div className={styles.galleryItem}>
+            <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRHhR9QiCnYQsr6WHvjH9iwwdX4d4Yy6mJiXjkgaYkgevErygD7INU-SpMHQwP0njWGnkE&usqp=CAU" alt="Image 2"/>
+        </div>
+        <div className={styles.galleryItem}>
+            <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRHhR9QiCnYQsr6WHvjH9iwwdX4d4Yy6mJiXjkgaYkgevErygD7INU-SpMHQwP0njWGnkE&usqp=CAU" alt="Image 2"/>
+        </div>
+        <div className={styles.galleryItem}>
+            <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRHhR9QiCnYQsr6WHvjH9iwwdX4d4Yy6mJiXjkgaYkgevErygD7INU-SpMHQwP0njWGnkE&usqp=CAU" alt="Image 2"/>
+        </div>
+        <div className={styles.galleryItem}>
+            <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRHhR9QiCnYQsr6WHvjH9iwwdX4d4Yy6mJiXjkgaYkgevErygD7INU-SpMHQwP0njWGnkE&usqp=CAU" alt="Image 2"/>
+        </div>
+        <div className={styles.galleryItem}>
+            <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRHhR9QiCnYQsr6WHvjH9iwwdX4d4Yy6mJiXjkgaYkgevErygD7INU-SpMHQwP0njWGnkE&usqp=CAU" alt="Image 2"/>
+        </div>
+        <div className={styles.galleryItem}>
+            <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRHhR9QiCnYQsr6WHvjH9iwwdX4d4Yy6mJiXjkgaYkgevErygD7INU-SpMHQwP0njWGnkE&usqp=CAU" alt="Image 2"/>
+        </div>
+        <div className={styles.galleryItem}>
+            <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRHhR9QiCnYQsr6WHvjH9iwwdX4d4Yy6mJiXjkgaYkgevErygD7INU-SpMHQwP0njWGnkE&usqp=CAU" alt="Image 2"/>
+        </div>
+        <div className={styles.galleryItem}>
+            <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRHhR9QiCnYQsr6WHvjH9iwwdX4d4Yy6mJiXjkgaYkgevErygD7INU-SpMHQwP0njWGnkE&usqp=CAU" alt="Image 2"/>
+        </div>
+    </div>
   )
 }
